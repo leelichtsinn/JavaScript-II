9fc5a214acba99f39e3b319b152794b5d1581556
leelichtsinn <lee.lichtsinn@gmail.com>
2018-08-16 22:52:22 -0400

    completed callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -11,28 +11,55 @@ function firstItem(arr, cb) {
 
 firstItem(items, itemLog);
 
-
+const arrLength = (e) => {
+  return console.log(e.length);
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+
+getLength(items, arrLength);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  let result = arr[arr.length-1];
+  return cb(result);
 }
+
+last(items, itemLog);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let result = x + y;
+  return cb(result);
 }
+
+sumNums(4, 5, itemLog);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = x * y;
+  return cb(result);
 }
+
+multiplyNums(4, 5, itemLog);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let result = false;
+  for (let i = 0; i < list.length; i++) {
+    if (list.indexOf(item) !== -1) {
+      result = true;
+    }
+  }
+  return cb(result);
 }
+
+contains("Gum", items, itemLog);
+contains("pizza", items, itemLog);
 
 /* STRETCH PROBLEM */
 
